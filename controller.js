82b650b8800d01217b4b92c3528418698a055001
Ryz <rjenvey@hotmail.co.uk>
2018-02-18 18:45:36 +0000

    remove need to reload page, add parameterised language and mobile optimisation

--- controller.js
+++ controller.js
@@ -1,16 +1,25 @@
-const tag = 'Romanian'
+const query = window.location.search.split('=')
+const tag = query.length > 1 ? query[1] : ''
 
 const notes = data.notes
   .filter(note => note.tags.indexOf(tag) > -1)
   .map(note => ({front: note.fields[0], back: note.fields[1]}))
 
 const getRandom = (myList) => myList[Math.floor(Math.random() * myList.length)]
-
-let note1 = getRandom(notes)
-let note2 = getRandom(notes)
-let note3 = getRandom(notes)
-
 const printAtId = (id, s) => document.getElementById(id).innerHTML = s
+
+let note1
+let note2
+let note3
+let backVisible
+
+const reset = () => {
+  note1 = getRandom(notes)
+  note2 = getRandom(notes)
+  note3 = getRandom(notes)
+  backVisible = false
+  printNotes()
+}
 
 const printNotes = (field = 'front') => {
   printAtId('note1', note1[field])
@@ -18,8 +27,19 @@ const printNotes = (field = 'front') => {
   printAtId('note3', note3[field])
 }
 
-printNotes()
-
-const showBack = () => {
-  printNotes('back')
+const showAnswerOrReset = () => {
+  if (!backVisible) {
+    printNotes('back')
+    backVisible = true
+  } else {
+    reset()
+  }
 }
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === 32 ){
+    showAnswerOrReset()
+  }
+})
+
+reset()
